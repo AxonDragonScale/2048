b517e7adee959e76339c7317c07ec5f4a1605c2b
Ronak Harkhani <ronak.harkhani@phonepe.com>
2021-06-05 02:28:47 +0530

    Layout Skeleton done.
    - Skeleton structure of header and board.
    - Basic colors for board and tiles.

--- app/src/main/java/com/axondragonscale/tzfe/ui/theme/Color.kt
+++ app/src/main/java/com/axondragonscale/tzfe/ui/theme/Color.kt
@@ -6,3 +6,21 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+object Colors {
+    val GameBackground = Color(251, 248, 240)
+    val BoardBackground = Color(183, 173, 162)
+
+    // Tile Colors
+    val TileEmpty = Color(197, 186, 175)
+    val Tile2 = Color(235, 228, 220)
+    val Tile4 = Color(233, 179, 204)
+    val Tile8 = Color(225, 179, 135)
+    val Tile16 = Color(215, 151, 112)
+    val Tile32 = Color(211, 131, 105)
+    val Tile64 = Color(214, 113, 85)
+
+    // Tile Fonts
+    val light = Color.White
+    val dark = Color(116, 110, 103)
+}
